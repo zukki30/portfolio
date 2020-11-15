5acf727bc081130cbb0922336d25dc90d0d378af
kk-watanabe <kazuki.watanabe@bebit.com>
2020-11-15 15:52:31 +0900

    feat: Add Stroybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    }
+  },
+  webpackFinal: async (baseConfig) => {
+    const nextConfig = require("../next.config.js");
+
+    // merge whatever from nextConfig into the webpack config storybook will use
+    return { ...baseConfig };
+  },
+}
