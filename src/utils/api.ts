7d8ce244ec1d46f8d6cb290e324dd7af89a36d76
kk-watanabe <kazuki.watanabe@bebit.com>
2020-11-20 20:42:09 +0900

    feat: Add api  interface

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,27 +2,78 @@ import fetch from "isomorphic-unfetch";
 import { API_URLS } from "@/const/Api";
 
 export interface Res {
-  contents: ResContent[];
+  contents: ResContents;
   totalCount: number;
   offset: number;
   limit: number;
 }
 
-export interface ResContent {
+type ResContents =
+  | OutputResContent[]
+  | ProfileResContent
+  | CurriculumVitaeResContent[];
+
+export interface OutputResContent extends ResContent {
+  title: string;
+  tags: string;
+  images: ResImage;
+  body: string;
+  chapter: ResChapter[];
+}
+
+export interface ProfileResContent {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  last_name: string;
+  first_name: string;
+  last_kana_name: string;
+  first_kana_name: string;
+  birthday: string;
+  address: string;
+  final_education: string;
+  graduation_year: string;
+  occupation: string;
+  slill: string;
+  future: string;
+  tool: string;
+  use_technology: string;
+  growth: string;
+  sex: string[];
+}
+
+export interface CurriculumVitaeResContent extends ResContent {
+  name: string;
+  period_start: string;
+  period_end?: undefined;
+  is_work: boolean;
+  body: string;
+  project: ResProject[];
+}
+
+interface ResContent {
   id: string;
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
-  [key: string]: string | ResImage | ResChapter[];
 }
 
-export interface ResImage {
+interface ResImage {
   url: string;
 }
 
-export interface ResChapter {
+interface ResChapter {
   fieldId: string;
   title: string;
+  body: string;
+}
+
+interface ResProject {
+  fieldId: string;
+  title: string;
+  period_start: string;
+  period_end: string;
+  experience: string[];
   body: string;
 }
 
