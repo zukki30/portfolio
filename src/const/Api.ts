e24a6bf1382156d19fced896fbd40e1213ae19fa
kk-watanabe <kazuki.watanabe@bebit.com>
2020-12-04 20:28:11 +0900

    feat: Add API_URL_DATAS const

--- src/const/Api.ts
+++ src/const/Api.ts
@@ -3,3 +3,20 @@ export enum API_URLS {
   CURRICULUM_VITAE = "curriculum-vitae",
   OUTPUT = "output",
 }
+
+export const API_URL_DATAS: {
+  [key: string]: { id: API_URLS; name: string };
+} = {
+  profile: {
+    id: API_URLS.PROFILE,
+    name: "プロフィール",
+  },
+  "curriculum-vitae": {
+    id: API_URLS.CURRICULUM_VITAE,
+    name: "職務経歴書",
+  },
+  output: {
+    id: API_URLS.OUTPUT,
+    name: "アウトプット",
+  },
+};
