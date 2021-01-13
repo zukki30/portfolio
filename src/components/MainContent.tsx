fcd0e982d734d907f3826e5ebca8eb690fe0b85a
kk-watanabe <kazuki.watanabe@bebit.com>
2021-01-13 19:33:57 +0900

    feat: Add MainContent components

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import { NextPage } from "next";
+
+import { MAIN_CONTENT } from "@/const/ElementTag";
+
+import { classNames } from "@/utils/components";
+
+import styles from "@/styles/components/MainContent.module.scss";
+
+interface Props {
+  element?: MAIN_CONTENT;
+  children?: ReactNode;
+  className?: string;
+}
+
+const MainContent: NextPage<Props> = (data) => {
+  const parentClass = classNames(styles.mainContent, data.className);
+  const Tag = data.element ? data.element : MAIN_CONTENT.Div;
+
+  return <Tag className={parentClass}>{data.children}</Tag>;
+};
+
+export default MainContent;
