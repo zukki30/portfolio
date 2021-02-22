d528e1b07b816f61ded03b7d037400ce439d6713
kk-watanabe <kazuki.watanabe@bebit.com>
2021-02-22 21:25:05 +0900

    feat: Add unit test to PageHead

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -3,36 +3,21 @@ import Head from "next/head";
 import { NextPage } from "next";
 import { SITE_NAME } from "@/const/site";
 
-interface Props {
+export interface Props {
   description: string;
-  image: string;
-  url: string;
   title?: string;
 }
 
-const PageHead: NextPage<Props> = (data) => {
+export const PageHead: NextPage<Props> = (data) => {
   const siteName = SITE_NAME;
   const pageTitle = data.title ? `${data.title} | ${siteName}` : siteName;
 
   return (
     <Head>
+      <meta name="robots" content="none" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content={data.description} />
       <title>{pageTitle}</title>
-      <meta property="og:title" content={pageTitle} />
-      <meta property="og:description" content={data.description} />
-      <meta property="og:type" content="blog" />
-      <meta property="og:url" content={data.url} />
-      <meta property="og:image" content={"https://placehold.jp/150x150.png"} />
-      <meta property="og:site_name" content={pageTitle} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@tcr_jp" />
-      <meta name="twitter:url" content={"https://placehold.jp/150x150.png"} />
-      <meta name="twitter:title" content={pageTitle} />
-      <meta name="twitter:description" content={data.description} />
-      <meta name="twitter:image" content={"https://placehold.jp/150x150.png"} />
-      <link rel="canonical" href={data.url} />
-      <link rel="shortcut icon" href={"https://placehold.jp/150x150.png"} />
-      <link rel="apple-touch-icon" href={"https://placehold.jp/150x150.png"} />
     </Head>
   );
 };
