20d4eb7d3dc0a885d3454bbacebc8e3a9ec5561e
kk-watanabe <kazuki.watanabe@bebit.com>
2021-02-24 18:14:53 +0900

    fix: Fix add class test to Logo

--- tests/components/Logo.spec.tsx
+++ tests/components/Logo.spec.tsx
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import { Logo, Props } from "@/components/Logo";
 import { SITE_NAME } from "@/const/site";
 
+const PARENT_CLASS = "parentClass";
 const IS_HOME = false;
 
 let props: Props;
@@ -11,7 +12,14 @@ describe("Logo", () => {
   beforeEach(() => {
     props = {
       isHome: IS_HOME,
+      className: PARENT_CLASS,
     };
+  });
+
+  it("Add parent class to `.logo`", () => {
+    const wrapper = shallow(<Logo {...props} />);
+
+    expect(wrapper.hasClass(PARENT_CLASS)).toBe(true);
   });
 
   it("Rendering of elements in a `.logo`", () => {
