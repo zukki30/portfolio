65f8f7fecce2da0462561805f1062e27a32f0dc1
zukki30 <kw4869ify@gmail.com>
2022-05-28 05:09:47 +0900

    feat: Add styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
 config.autoAddCss = false;
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
