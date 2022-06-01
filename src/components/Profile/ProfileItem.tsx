5558371c92e7dd0018c56e2bbfa843fefe7be3ca
zukki30 <kw4869ify@gmail.com>
2022-06-02 02:49:06 +0900

    feat: Add ProfileItem components

--- src/components/Profile/ProfileItem.tsx
+++ src/components/Profile/ProfileItem.tsx
@@ -1,16 +1,59 @@
-import { ReactNode } from "react";
 import { NextPage } from "next";
 import Image from "next/image";
+import styled from "styled-components";
+
+import { Profile } from "@/types";
+import { GreenColors } from "@/consts/color";
+import { FixedSizes, FontSizes } from "@/consts/size";
+import Card from "@/components/Elements/Card";
 
 export interface ProfileItemProps {
-  children?: ReactNode;
+  profile: Profile;
 }
 
+const ProfileItemContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileItemImage = styled.div`
+  border-radius: ${FixedSizes[8]};
+  overflow: hidden;
+`;
+
+const ProfileItemLabel = styled.h2`
+  color: ${GreenColors[600]};
+  font-size: ${FontSizes.lg};
+  font-weight: bold;
+  line-height: 1.3;
+
+  &:not(:first-child) {
+    margin-top: ${FixedSizes[20]};
+  }
+`;
+
+const ProfileItemText = styled.p`
+  margin-top: ${FixedSizes[16]};
+  line-height: 1.5;
+`;
+
 export const ProfileItem: NextPage<ProfileItemProps> = (props) => {
+  const { label, body, image } = props.profile;
+  const showImage = image !== null;
+
   return (
-    <div>
-      <Image src='https://placehold.jp/150x150.png' width={150} height={150} />
-    </div>
+    <Card>
+      <ProfileItemContainer>
+        {showImage && (
+          <ProfileItemImage>
+            <Image src='https://placehold.jp/150x150.png' width={150} height={150} />
+          </ProfileItemImage>
+        )}
+        <ProfileItemLabel>{label}</ProfileItemLabel>
+        <ProfileItemText>{body}</ProfileItemText>
+      </ProfileItemContainer>
+    </Card>
   );
 };
 
--- src/stories/Profile/ProfileItem.stories.tsx
+++ src/stories/Profile/ProfileItem.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
+import { Profile } from "@/types";
 import ProfileItem from "@/components/Profile/ProfileItem";
 
 export default {
@@ -11,6 +11,28 @@ export default {
 
 const Template: ComponentStory<typeof ProfileItem> = (args) => <ProfileItem {...args} />;
 
-export const Default = Template.bind({});
+export const NotImage = Template.bind({});
 
-Default.args = {};
+NotImage.args = {
+  profile: {
+    id: "test1",
+    label: "プロフィールラベルプロフィールラベルプロフィールラベル",
+    body: "サンプルテキストサンプルテキストサンプルテキストサンプルテキスト",
+    image: null,
+  } as Profile,
+};
+
+export const ImageTrue = Template.bind({});
+
+ImageTrue.args = {
+  profile: {
+    id: "test1",
+    label: "プロフィールラベルプロフィールラベルプロフィールラベル",
+    body: "サンプルテキストサンプルテキストサンプルテキストサンプルテキスト",
+    image: {
+      url: "https://placehold.jp/150x150.png",
+      width: 150,
+      height: 150,
+    },
+  } as Profile,
+};
