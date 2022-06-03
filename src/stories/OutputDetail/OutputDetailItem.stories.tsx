e042825e2bd43dacb8505360d0cfbe9fec32eca8
zukki30 <kw4869ify@gmail.com>
2022-06-03 16:33:43 +0900

    feat: Add OutputDetailItem components

--- src/stories/OutputDetail/OutputDetailItem.stories.tsx
+++ src/stories/OutputDetail/OutputDetailItem.stories.tsx
@@ -13,4 +13,40 @@ const Template: ComponentStory<typeof OutputDetailItem> = (args) => <OutputDetai
 
 export const Default = Template.bind({});
 
-Default.args = {};
+const output = {
+  id: "testId",
+  title: "アウトプットタイトル",
+  image: {
+    url: "https://placehold.jp/1354x960.png",
+    width: 1354,
+    height: 960,
+  },
+  pageUrl: "https://www.google.com/?hl=ja",
+  startDate: "2019-12-31T15:00:00.000Z",
+  endDate: "",
+  content: "サンプルテキストサンプルテキスト\nサンプルテキストサンプルテキストサンプルテキストサンプルテキスト\n",
+  frontEndInfo: {
+    repositoryUrl: "https://www.google.com/?hl=ja",
+    skills: ["NuxtJS", "Vue.js", "HTML", "CSS", "Sass/SCSS", "JavaScript", "TypeScript", "Storybook"],
+    content:
+      "サンプルテキストサンプルテキスト\nサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト\nサンプルテキストサンプルテキストサンプルテキストサンプルテキスト",
+    beforeUpdates: [
+      {
+        title: "サイト公開",
+        content:
+          "サンプルテキストサンプルテキストサンプルテキストサンプルテキスト\nサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト",
+        skills: ["NuxtJS", "Vue.js", "HTML", "CSS", "Sass/SCSS", "JavaScript", "TypeScript", "Storybook"],
+      },
+    ],
+  },
+  backEndInfo: {
+    repositoryUrl: "https://www.google.com/?hl=ja",
+    skills: ["WordPress"],
+    content: "nサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト",
+    beforeUpdates: [],
+  },
+};
+
+Default.args = {
+  output,
+};
