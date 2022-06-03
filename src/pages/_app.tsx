1c345b20b263b5c8486f41d6eb9b03c8b6a6482e
zukki30 <kw4869ify@gmail.com>
2022-06-04 02:22:25 +0900

    feat: Add Loading to app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,51 @@
+import { useState, useEffect } from "react";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "reset-css";
 
+import Loading from "@/components/Elements/Loading";
+
 config.autoAddCss = false;
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const router = useRouter();
+  const getLayout = Component.getLayout || ((page) => page);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) => url !== router.asPath && setIsLoading(true);
+    const handleComplete = () => setIsLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, []);
+
   return (
     <>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
+
+      {isLoading && <Loading />}
     </>
   );
-}
+};
 
 export default MyApp;
